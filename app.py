9a428751aec1e8422f659880fc30fd02d83a9983
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2024-02-12 14:49:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,request,jsonify  #get () is used
+from model import predict_model
+app=Flask(__name__)
+
+@app.route('/')
+def basic():
+    return 'API Server is Started'
+
+@app.route('/soil', methods=['get'])
+def soil():
+    value=request.args.get('soil')
+    print(value)
+    value=int(value)
+    response=predict_model(value)
+    print(response)
+    return ({'msg':(response)})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=5001,debug=True)
